8a736b5962b2c2cd6252896ea2eff841cd3c384e
Millena Junior <millenajunior_bsit@plmun.edu.ph>
2025-03-01 21:18:40 +0800

    Update 01_folder_check.sh

--- 01_folder_check.sh
+++ 01_folder_check.sh
@@ -1,5 +1,6 @@
 Use `UniversityDB`;
-
+declare -a SQL_FILES
+SQL_FILES=("create_and_use_db.sql")
 CREATE TABLE `Students` (
    `StudentID` INT PRIMARY KEY AUTO_INCREMENT,
    `FirstName` VARCHAR(50) NOT NULL,
